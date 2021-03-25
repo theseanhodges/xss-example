0bcc43f1c6cb15714378b08e99335a52e266487d
Sean Hodges <sthodges@gmail.com>
2021-03-24 17:37:03 -0700

    Escape html control characters from user input

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import base64
+import html
 
 from flask import Flask, request
 from model import Message 
@@ -10,7 +11,7 @@ app = Flask(__name__)
 def home():
 
     if request.method == 'POST':
-        m = Message(content=request.form['content'])
+        m = Message(content=html.escape(request.form['content']))
         m.save()
 
     body = """
